31083f158a37324d1d02f0eac31164d5643f935b
Francis De Brabandere <francisdb@gmail.com>
2025-08-15 11:09:05 +0200

    style: clippy updates (#23)

--- src/music.rs
+++ src/music.rs
@@ -39,15 +39,15 @@ fn music_startup(asset_server: Res<AssetServer>, mut commands: Commands) {
 }
 
 fn music_update(music_box_query: Query<&AudioSink>, keys: Res<ButtonInput<KeyCode>>) {
-    if let Ok(sink) = music_box_query.single() {
-        if keys.just_pressed(KeyCode::KeyM) {
-            if sink.is_paused() {
-                info!("Playing music");
-                sink.play()
-            } else {
-                info!("Pausing music");
-                sink.pause()
-            }
+    if let Ok(sink) = music_box_query.single()
+        && keys.just_pressed(KeyCode::KeyM)
+    {
+        if sink.is_paused() {
+            info!("Playing music");
+            sink.play()
+        } else {
+            info!("Pausing music");
+            sink.pause()
         }
     }
 }
